80a67307f8e239fc78da9b43cbfc0aeb13f76d8a
Shaobin-Jiang <shaobin-jiang@outlook.com>
2023-12-16 15:37:47 +0800

    feat(plugins): update git keymap

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -200,13 +200,13 @@ config.gitsigns = {
     main = "gitsigns",
     opts = {},
     keys = {
-        { "<leader>ghn", ":Gitsigns next_hunk<CR>", desc = "next hunk", silent = true, noremap = true },
-        { "<leader>ghp", ":Gitsigns prev_hunk<CR>", desc = "prev hunk", silent = true, noremap = true },
-        { "<leader>gph", ":Gitsigns preview_hunk<CR>", desc = "preview hunk", silent = true, noremap = true },
-        { "<leader>gsh", ":Gitsigns stage_hunk<CR>", desc = "stage hunk", silent = true, noremap = true },
-        { "<leader>guh", ":Gitsigns undo_stage_hunk<CR>", desc = "undo stage", silent = true, noremap = true },
-        { "<leader>grh", ":Gitsigns reset_hunk<CR>", desc = "reset hunk", silent = true, noremap = true },
-        { "<leader>gsb", ":Gitsigns stage_buffer<CR>", desc = "stage buffer", silent = true, noremap = true },
+        { "<leader>gn", ":Gitsigns next_hunk<CR>", desc = "next hunk", silent = true, noremap = true },
+        { "<leader>gp", ":Gitsigns prev_hunk<CR>", desc = "prev hunk", silent = true, noremap = true },
+        { "<leader>gP", ":Gitsigns preview_hunk<CR>", desc = "preview hunk", silent = true, noremap = true },
+        { "<leader>gs", ":Gitsigns stage_hunk<CR>", desc = "stage hunk", silent = true, noremap = true },
+        { "<leader>gu", ":Gitsigns undo_stage_hunk<CR>", desc = "undo stage", silent = true, noremap = true },
+        { "<leader>gr", ":Gitsigns reset_hunk<CR>", desc = "reset hunk", silent = true, noremap = true },
+        { "<leader>gb", ":Gitsigns stage_buffer<CR>", desc = "stage buffer", silent = true, noremap = true },
     },
 }
 
@@ -320,7 +320,7 @@ config.neogit = {
         },
     },
     keys = {
-        { "<leader>ng", ":Neogit<CR>", desc = "neogit", silent = true, noremap = true },
+        { "<leader>gt", ":Neogit<CR>", desc = "neogit", silent = true, noremap = true },
     },
 }
 

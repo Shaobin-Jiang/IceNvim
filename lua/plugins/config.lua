f9b1d3749a044c7a99fb04ca7bd016a879571f41
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-01-03 13:22:04 +0800

    feat(plugins): require confirmation when closing modified buffers

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -19,7 +19,7 @@ config.bufferline = {
     priority = priority.HIGH,
     opts = {
         options = {
-            close_command = "bdelete! %d",
+            close_command = ":BufferLineClose %d",
             right_mouse_command = "bdelete! %d",
             separator_style = "thin",
             offsets = {
@@ -42,12 +42,33 @@ config.bufferline = {
         },
     },
     config = function(_, opts)
+        vim.api.nvim_create_user_command("BufferLineClose", function(opts)
+            local bufnr = 1 * opts.args
+            local buf_is_modified = vim.api.nvim_buf_get_option(bufnr, "modified")
+
+            local bdelete_arg
+            if bufnr == 0 then
+                bdelete_arg = ""
+            else
+                bdelete_arg = " " .. bufnr
+            end
+            local command = "bdelete!" .. bdelete_arg
+            if buf_is_modified then
+                local option = vim.fn.confirm("File is not saved. Close anyway?", "&Yes\n&No", 2)
+                if option == 1 then
+                    vim.cmd(command)
+                end
+            else
+                vim.cmd(command)
+            end
+        end, { nargs = 1 })
+
         require("bufferline").setup(opts)
     end,
     keys = {
         { "<leader>bc", ":BufferLinePickClose<CR>", desc = "pick close", silent = true, noremap = true },
         -- <esc> is added in case current buffer is the last
-        { "<leader>bd", ":bdelete!<CR><Esc>", desc = "close current buffer", silent = true, noremap = true },
+        { "<leader>bd", ":BufferLineClose 0<CR><ESC>", desc = "close current buffer", silent = true, noremap = true },
         { "<leader>bh", ":BufferLineCyclePrev<CR>", desc = "prev buffer", silent = true, noremap = true },
         { "<leader>bl", ":BufferLineCycleNext<CR>", desc = "next buffer", silent = true, noremap = true },
         { "<leader>bo", ":BufferLineCloseOthers<CR>", desc = "close others", silent = true, noremap = true },

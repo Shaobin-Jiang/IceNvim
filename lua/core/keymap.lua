d809d6245eb2d2aac3f87b5bc63a8d4c3418197d
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-02-12 20:18:58 +0800

    fix(core): use `mode` to clear command line
    There occasions when the old way of clearing the command line, i.e.,
    echoing an empty string, would not do the trick. Redrawing would be much
    more safe-proof

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -36,7 +36,7 @@ Ice.keymap = {}
 Ice.keymap.general = {
     -- See `:h quote_`
     black_hole_register = { { "n", "v" }, "\\", '"_' },
-    clear_cmd_line = { "n", "<C-g>", ":echo<CR>", { noremap = true } },
+    clear_cmd_line = { { "n", "i", "v", "t" }, "<C-g>", "<Cmd>mode<CR>", { noremap = true } },
     cmd_forward = { "c", "<C-f>", "<Right>", { silent = false } },
     cmd_backward = { "c", "<C-b>", "<Left>", { silent = false } },
     cmd_home = { "c", "<C-a>", "<Home>", { silent = false } },

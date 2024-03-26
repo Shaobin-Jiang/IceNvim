50b898770819ce82a77899123ad27d5d9636bc08
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-03-26 20:54:27 +0800

    perf(plugins): lazy load bufferline
    It had not been possible to lazy load bufferline as it needed to be
    loaded before nvim-transparent, which must not be lazy loaded, so that
    the bufferline would not appear transparent. However, by copying the
    opaque Normal highlight group to a new IceNormal highlight group, and by
    modifying nvim_get_hl to point to IceNormal if it is querying the Normal
    highlight group, it is now possible to make bufferline opaque even when
    lazy loading it after nvim-transparent.

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -11,7 +11,6 @@ local priority = {
 
 config.bufferline = {
     "akinsho/bufferline.nvim",
-    lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
@@ -437,6 +436,18 @@ config["nvim-transparent"] = {
         },
     },
     config = function(_, opts)
+        local autogroup = vim.api.nvim_create_augroup("transparent", { clear = true })
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            group = autogroup,
+            callback = function()
+                local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+                local foreground = string.format("#%06x", normal_hl.fg)
+                local background = string.format("#%06x", normal_hl.bg)
+                vim.api.nvim_command("highlight default IceNormal guifg=" .. foreground .. " guibg=" .. background)
+
+                require("transparent").clear()
+            end,
+        })
         -- Enable transparent by default
         local transparent_cache = vim.fn.stdpath "data" .. "/transparent_cache"
         local f = io.open(transparent_cache, "r")
@@ -449,6 +460,17 @@ config["nvim-transparent"] = {
         end
 
         require("transparent").setup(opts)
+
+        local old_get_hl = vim.api.nvim_get_hl
+        vim.api.nvim_get_hl = function(ns_id, opt)
+            if opt.name == "Normal" then
+                local attempt = old_get_hl(0, { name = "IceNormal" })
+                if next(attempt) ~= nil then
+                    opt.name = "IceNormal"
+                end
+            end
+            return old_get_hl(ns_id, opt)
+        end
     end,
 }
 

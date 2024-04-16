581d8c30b80b8a383588ad23e98c7e48c8936c48
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-04-16 10:39:26 +0800

    fix(plugins): do not load Comment during config

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -137,7 +137,6 @@ config.comment = {
     end,
     keys = function()
         local vvar = vim.api.nvim_get_vvar
-        local api = require "Comment.api"
 
         local toggle_current_line = function()
             if vvar "count" == 0 then
@@ -155,6 +154,18 @@ config.comment = {
             end
         end
 
+        local comment_below = function()
+            require("Comment.api").insert.linewise.below()
+        end
+
+        local comment_above = function()
+            require("Comment.api").insert.linewise.above()
+        end
+
+        local comment_eol = function()
+            require("Comment.api").locked "insert.linewise.eol"()
+        end
+
         return {
             { "<leader>c", "<Plug>(comment_toggle_linewise)", desc = "comment toggle linewise" },
             { "<leader>ca", "<Plug>(comment_toggle_blockwise)", desc = "comment toggle blockwise" },
@@ -162,9 +173,9 @@ config.comment = {
             { "<leader>cb", toggle_current_block, expr = true, desc = "comment toggle current block" },
             { "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", mode = "x", desc = "comment toggle linewise" },
             { "<leader>cb", "<Plug>(comment_toggle_blockwise_visual)", mode = "x", desc = "comment toggle blockwise" },
-            { "<leader>co", api.insert.linewise.below, desc = "comment insert below" },
-            { "<leader>cO", api.insert.linewise.above, desc = "comment insert above" },
-            { "<leader>cA", api.locked "insert.linewise.eol", desc = "comment insert end of line" },
+            { "<leader>co", comment_below, desc = "comment insert below" },
+            { "<leader>cO", comment_above, desc = "comment insert above" },
+            { "<leader>cA", comment_eol, desc = "comment insert end of line" },
         }
     end,
 }

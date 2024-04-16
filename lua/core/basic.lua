4434c418fa60fb6d05460bfe53eb8e243d5d004f
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-04-16 17:18:37 +0800

    perf(core): postpone reading of shada file

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -90,3 +90,15 @@ opt.shellslash = true
 vim.cmd [[
     autocmd TermOpen * setlocal nonumber norelativenumber
 ]]
+
+opt.shadafile = "NONE"
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+    once = true,
+    callback = function()
+        local shada = vim.fn.stdpath("state") .. "/shada/main.shada"
+        vim.o.shadafile = shada
+        vim.api.nvim_command("rshada! " .. shada)
+    end,
+})
+
+-- vim.cmd "syntax off"

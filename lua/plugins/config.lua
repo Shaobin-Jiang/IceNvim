b07938da948fea8ecc98d1e584510f6a6658f353
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-05-07 13:07:55 +0800

    add vimdoc to nvim-treesitter ensure_install list

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -651,6 +651,7 @@ config["nvim-treesitter"] = {
             "typescript",
             "tsx",
             "vim",
+            "vimdoc",
         },
         highlight = {
             enable = true,

9df17e23ccc8941da584c700f52ba75dcdd00bd2
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-05-09 10:26:00 +0800

    perf(plugins): disable treesitter for 100+ kb files

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -658,6 +658,13 @@ config["nvim-treesitter"] = {
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
+            disable = function(_, buf)
+                local max_filesize = 100 * 1024
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
         },
         incremental_selection = {
             enable = true,

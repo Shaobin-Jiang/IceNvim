35f52b7ee8756e741cd6a466b3317d5f0fd01d96
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-07-31 15:51:52 +0800

    set tabstop to 2 to match defaults of typstfmt

--- lua/core/ft.lua
+++ lua/core/ft.lua
@@ -37,5 +37,8 @@ Ice.ft = {
         vim.wo.wrap = true
         vim.wo.linebreak = true
         vim.wo.breakindent = true
+        vim.bo.shiftwidth = 2
+        vim.bo.softtabstop = 2
+        vim.bo.tabstop = 2
     end
 }

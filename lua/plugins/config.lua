50d7e765c9c9b79e48b2f8fa524e7bddb3491094
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-08-19 20:27:24 +0800

    move markdown plugin back to plugins/
    This does not belong in the section of lsp

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -353,6 +353,26 @@ config.lualine = {
     },
 }
 
+config["markdown-preview"] = {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    config = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_auto_close = 0
+    end,
+    build = "cd app && yarn install",
+    keys = {
+        {
+            "<A-b>",
+            "<Cmd>MarkdownPreviewToggle<CR>",
+            desc = "markdown preview",
+            ft = "markdown",
+            silent = true,
+            noremap = true,
+        },
+    },
+}
+
 config.neogit = {
     "NeogitOrg/neogit",
     dependencies = "nvim-lua/plenary.nvim",

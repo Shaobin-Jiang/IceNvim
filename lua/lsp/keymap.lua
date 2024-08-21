e52fa7dac51333607c47cabb44127a439a7d42c9
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-08-21 17:04:41 +0800

    feat(lsp): move cursor into hover doc after showing up
    This enables better reading experience.

--- lua/lsp/keymap.lua
+++ lua/lsp/keymap.lua
@@ -4,7 +4,22 @@ Ice.keymap.lsp.mapLsp = {
     rename = { "n", "<leader>lr", "<Cmd>Lspsaga rename<CR>" },
     code_action = { "n", "<leader>lc", "<Cmd>Lspsaga code_action<CR>" },
     go_to_definition = { "n", "<leader>ld", "<Cmd>Lspsaga goto_definition<CR>" },
-    doc = { "n", "<leader>lh", "<Cmd>Lspsaga hover_doc<CR>" },
+    hover_doc = {
+        "n",
+        "<leader>lh",
+        function()
+            vim.cmd "Lspsaga hover_doc"
+
+            -- PERF: maybe we can do better at this? For now I have now figured out how to make this work without the
+            -- delay, though.
+            vim.defer_fn(function()
+                local winid = require("lspsaga.hover").winid
+                if winid then
+                    vim.api.nvim_set_current_win(winid)
+                end
+            end, 100)
+        end,
+    },
     references = { "n", "<leader>lR", "<Cmd>Lspsaga finder<CR>" },
     go_to_implementation = { "n", "<leader>li", "<Cmd>Lspsaga finder<CR>" },
     show_line_diagnostic = { "n", "<leader>lP", "<Cmd>Lspsaga show_line_diagnostics<CR>" },

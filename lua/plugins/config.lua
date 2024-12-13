43ca33f5c0d2489b2d75d72eccfa322407694a47
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-12-13 15:46:36 +0800

    fix typo

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -11,7 +11,7 @@ local priority = {
 
 -- Add IceLoad event
 -- If user starts neovim but does not edit a file, i.e., entering Dashboard directly, the IceLoad event is hooked to the
--- next BufRead event. Otherwise, the event is triggered right after the VeryLazy event.
+-- next BufEnter event. Otherwise, the event is triggered right after the VeryLazy event.
 vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()

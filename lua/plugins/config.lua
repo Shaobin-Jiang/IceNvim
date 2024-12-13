0f6925e42881a492987011c4e15666932c3b0d8e
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-12-13 21:07:26 +0800

    feat(plugins): force dashboard footer to be non-italic

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -247,6 +247,19 @@ config.dashboard = {
     },
     config = function(_, opts)
         require("dashboard").setup(opts)
+
+        -- Force the footer to be non-italic
+        -- Dashboard loads before the colorscheme plugin, so we should defer the setting of the highlight group to when
+        -- all plugins are finished loading
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VeryLazy",
+            once = true,
+            callback = function()
+                -- Use the highlight command to replace instead of overriding the original highlight group
+                -- Much more convenient than using vim.api.nvim_set_hl()
+                vim.cmd "highlight DashboardFooter cterm=NONE gui=NONE"
+            end,
+        })
     end,
 }
 

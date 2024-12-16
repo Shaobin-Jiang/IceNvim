6c42bba5dde79ef814e018973299114c913d1a8a
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-12-16 17:32:45 +0800

    move cmp keymap setup to lsp.utils

--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -49,44 +49,7 @@ Ice.plugins["nvim-cmp"] = {
         }
 
         local cmp = require "cmp"
-
-        local cmp_func = {
-            toggle_completion = cmp.mapping {
-                i = function()
-                    if cmp.visible() then
-                        cmp.abort()
-                    else
-                        cmp.complete()
-                    end
-                end,
-                c = function()
-                    if cmp.visible() then
-                        cmp.close()
-                    else
-                        cmp.complete()
-                    end
-                end,
-            },
-            prev_item = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-            next_item = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-            confirm = cmp.mapping(
-                cmp.mapping.confirm {
-                    select = true,
-                    behavior = cmp.ConfirmBehavior.Insert,
-                },
-                { "i", "c" }
-            ),
-            doc_up = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i" }),
-            doc_down = cmp.mapping(cmp.mapping.scroll_docs(4), { "i" }),
-        }
-
-        local cmp_keymap = {}
-        for command, key in pairs(Ice.keymap.lsp.cmp) do
-            local func = cmp_func[command]
-            if func then
-                cmp_keymap[key] = func
-            end
-        end
+        local cmp_keymap = require("lsp.utils").create_cmp_keymap()
 
         cmp.setup {
             snippet = {
--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -1,5 +1,48 @@
 local utils = {}
 
+utils.create_cmp_keymap = function()
+    local cmp = require "cmp"
+    local cmp_func = {
+        toggle_completion = cmp.mapping {
+            i = function()
+                if cmp.visible() then
+                    cmp.abort()
+                else
+                    cmp.complete()
+                end
+            end,
+            c = function()
+                if cmp.visible() then
+                    cmp.close()
+                else
+                    cmp.complete()
+                end
+            end,
+        },
+        prev_item = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        next_item = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        confirm = cmp.mapping(
+            cmp.mapping.confirm {
+                select = true,
+                behavior = cmp.ConfirmBehavior.Insert,
+            },
+            { "i", "c" }
+        ),
+        doc_up = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i" }),
+        doc_down = cmp.mapping(cmp.mapping.scroll_docs(4), { "i" }),
+    }
+
+    local cmp_keymap = {}
+    for command, key in pairs(Ice.keymap.lsp.cmp) do
+        local func = cmp_func[command]
+        if func then
+            cmp_keymap[key] = func
+        end
+    end
+
+    return cmp_keymap
+end
+
 utils.lsp_attach_keymap = function(bufnr)
     require("core.utils").group_map(Ice.keymap.lsp.mapLsp, { noremap = true, silent = true, buffer = bufnr })
 end

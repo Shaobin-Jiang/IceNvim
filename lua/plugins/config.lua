581a951e5a897c24347f3577c4e7adb4f44c543a
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-12-17 21:29:53 +0800

    add <leader>o prefix to which-key

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -898,6 +898,7 @@ config["which-key"] = {
             { "<leader>g", group = "+git" },
             { "<leader>h", group = "+hop" },
             { "<leader>l", group = "+lsp" },
+            { "<leader>o", group = "+orgmode" },
             { "<leader>t", group = "+telescope" },
             { "<leader>u", group = "+utils" },
         },

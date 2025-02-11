5bd00443680b889f5abadd5c9c11b0822876652e
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-02-11 15:51:39 +0800

    rewrite check_icons without nui / replace it with IceCheckIcons command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,4 +1,64 @@
+vim.api.nvim_create_user_command("IceCheckIcons", function()
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.keymap.set("n", "q", "<C-w>c", { buffer = buf })
+
+    local item_width = 24
+    local item_name_width = 18
+    local win_width = vim.fn.winwidth(0)
+    local win_height = vim.fn.winheight(0)
+    local columns = math.floor(win_width / item_width) - 1
+
+    local content = {}
+    local items_in_row = 0
+    local line = ""
+    local item_number = 0
+    for name, icon in require("core.utils").ordered_pair(Ice.symbols) do
+        item_number = item_number + 1
+        line = string.format(
+            "%s%s%s%s%s",
+            line,
+            name,
+            string.rep(" ", item_name_width - #name),
+            icon,
+            string.rep(" ", item_width - item_name_width - vim.fn.strdisplaywidth(icon))
+        )
+
+        items_in_row = items_in_row + 1
+
+        if items_in_row == columns then
+            content[#content + 1] = line
+            items_in_row = 0
+            line = ""
+        end
+    end
+
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, content)
+
+    local width = columns * item_width
+    local height = math.ceil(item_number / columns)
+    local left = math.floor((win_width - width) / 2)
+    local top = math.floor((win_height - height) / 2)
+
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "win",
+        width = width,
+        height = height,
+        row = top,
+        col = left,
+        border = "rounded",
+        title = "Check Nerd Font Icons",
+        title_pos = "center",
+        footer = "Press q to close window",
+        footer_pos = "center",
+    })
+
+    vim.api.nvim_set_option_value("number", false, { win = win })
+    vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+    vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+end, { nargs = 0 })
+
 vim.api.nvim_create_user_command("IceUpdate", "lua require('core.utils').update()", { nargs = 0 })
+
 vim.api.nvim_create_user_command("IceHealth", "checkhealth core", { nargs = 0 })
 
 -- Allow a command to be repeated based on v:count1
--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -1,12 +1,7 @@
 local utils = require "plugins.utils"
 
 Ice.keymap.plugins = {
-    check_icons = { "n", "<leader>ui", utils.check_icons },
-    lazy_profile = {
-        "n",
-        "<leader>ul",
-        "<Cmd>Lazy profile<CR>",
-    },
+    lazy_profile = { "n", "<leader>ul", "<Cmd>Lazy profile<CR>" },
     select_colorscheme = { "n", "<C-k><C-t>", utils.select_colorscheme },
     view_configuration = { "n", "<leader>uc", utils.view_configuration },
 }

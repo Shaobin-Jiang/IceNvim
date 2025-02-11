982b3ee64dc13b7acb78bc77d6b2928971bd445c
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-02-11 14:57:51 +0800

    chore(core): move Ice-* commands to core.commands

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,5 @@
 require "core.basic"
+require "core.commands"
 require "core.ft"
 require "core.keymap"
 require "core.miscellaneous"
--- lua/core/miscellaneous.lua
+++ lua/core/miscellaneous.lua
@@ -127,33 +127,3 @@ if utils.is_windows() then
         end,
     })
 end
-
-vim.api.nvim_create_user_command("IceUpdate", "lua require('core.utils').update()", { nargs = 0 })
-vim.api.nvim_create_user_command("IceHealth", "checkhealth core", { nargs = 0 })
-
--- Allow a command to be repeated based on v:count1
-vim.api.nvim_create_user_command("IceRepeat", function(args)
-    for _ = 1, vim.v.count1 do
-        vim.cmd(args.args)
-    end
-end, { nargs = "+", complete = "command" })
-
--- View the output of a command in an external buffer
-vim.api.nvim_create_user_command("IceView", function(args)
-    local path = vim.fn.stdpath "data" .. "/ice-view.txt"
-    if args.args == "" then
-        vim.cmd("edit " .. path)
-    else
-        vim.cmd(string.format(
-            [[
-                redir! > %s
-                silent %s
-                redir END
-                edit %s
-            ]],
-            path,
-            args.args,
-            path
-        ))
-    end
-end, { nargs = "*", complete = "command" })

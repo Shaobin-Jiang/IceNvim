8879d8be28720ed62f20368fa7ce5ecd08126030
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-03-31 10:59:47 +0800

    feat(plugins): remove neoscroll plugin

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -409,38 +409,6 @@ config.neogit = {
     end,
 }
 
-config.neoscroll = {
-    "karb94/neoscroll.nvim",
-    main = "neoscroll",
-    opts = {
-        mappings = {},
-        hide_cursor = true,
-        stop_eof = true,
-        respect_scrolloff = false,
-        cursor_scrolls_alone = true,
-        easing_function = "sine",
-        pre_hook = nil,
-        post_hook = nil,
-        performance_mode = false,
-    },
-    keys = {
-        {
-            "<C-u>",
-            function()
-                require("neoscroll").scroll(-vim.wo.scroll, true, 250)
-            end,
-            desc = "scroll up",
-        },
-        {
-            "<C-d>",
-            function()
-                require("neoscroll").scroll(vim.wo.scroll, true, 250)
-            end,
-            desc = "scroll down",
-        },
-    },
-}
-
 config.nui = {
     "MunifTanjim/nui.nvim",
     lazy = true,
